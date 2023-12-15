1be7f60cfd0d7c00215b4370746aff249b5ccf16
Savelieva <krasilnikova.katerina@gmail.com>
2023-12-15 15:52:32 +0200

    + Users api without midelwares

--- services/userService.js
+++ services/userService.js
@@ -3,6 +3,35 @@ import { userRepository } from "../repositories/userRepository.js";
 class UserService {
   // TODO: Implement methods to work with user
 
+
+  getAll() {
+    const users = userRepository.getAll();
+    return users;
+  }
+
+  getById(userId) {
+    const user = this.search({ id: userId })
+    return user;
+  }
+
+  create(data) {
+    const user = userRepository.create(data);
+    return user;
+  }
+
+  update(id, data) {
+    const user = userRepository.update(id, data);
+    return user;
+  }
+
+  delete(id) {
+    const user = this.getById(id);
+    if (!user) {
+      throw Error(`There is no user with id = '${id}'`)
+    }
+    userRepository.delete(id);
+    return user;
+  }
   search(search) {
     const item = userRepository.getOne(search);
     if (!item) {
